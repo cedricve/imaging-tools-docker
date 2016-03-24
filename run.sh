8b99d9df5e6d7c7e9fccd20fd4c2f26d8f04ab9d
Scott Kidder <skidder@brightcove.com>
2016-03-24 13:36:36 -0700

    Install libjpeg separately from the development dependencies

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ export MAKEFLAGS="-j$[$(nproc) + 1]"
 export SRC=/usr/local
 export PKG_CONFIG_PATH=${SRC}/lib/pkgconfig
 
-yum install -y autoconf automake gcc gcc-c++ git libtool make nasm zlib-devel openssl-devel tar cmake perl which bzip2 libjpeg libjpeg-devel
+yum install -y libjpeg
+yum install -y autoconf automake gcc gcc-c++ git libtool make nasm zlib-devel openssl-devel tar cmake perl which bzip2 libjpeg-devel
 
 # yasm
 DIR=$(mktemp -d) && cd ${DIR} && \
